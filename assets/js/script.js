d3388cb3bb2a1acf4f72d187bc57611c37b3030d
andrew.opitz222@gmail.com <andrew.opitz@yahoo.com>
2023-09-26 15:54:28 -0400

    added functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,43 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
+  var timeBlocks = $('.time-block')
+  var currentTime = dayjs().format('dddd, MMMM DD, YYYY')
+  $('#currentDay').text(currentTime)
+  var currentHour = dayjs().hour()
+  console.log(currentHour)
+function pageResetter() {
+  timeBlocks.each(function() {
+    var timeBlock = $(this)
+
+    var hour = parseInt(timeBlock.attr('id').split('-')[1])
+    console.log(hour)
+    if(hour < currentHour) {
+      $(this).addClass('past')
+    } else if (hour === currentHour) {
+      $(this).removeClass('past')
+      $(this).addClass('present')
+    } else {
+      $(this).removeClass('past')
+      $(this).removeClass('present')
+      $(this).addClass('future')
+    }
+  })
+
+  for(var i = 9; i < 18; i++) {
+    $(`#hour-${i} .description`).val(localStorage.getItem(`hour-${i}`))
+  }
+}
+setInterval(900000, pageResetter)
+  $('.saveBtn').click(function(e) {
+    var hourKey = $(this).parent().attr('id')
+    var activity = $(this).siblings('.description').val()
+    localStorage.setItem(hourKey, activity)
+    $(this).siblings('p').removeClass('hide')
+    
+
+  })
+  pageResetter()
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
